42ea6fbaffb0b3aca2dca28e6cfd1cfcba27bbe4
XuXianda <xuxd@landicorp.com>
2018-04-16 17:57:44 +0800

    日志功能

--- springboot-22/src/main/java/com/xuxianda/Application.java
+++ springboot-22/src/main/java/com/xuxianda/Application.java
@@ -6,6 +6,9 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * Created by Xianda Xu on 2018/4/15.
+ * springboot的默认日志级别是info
+ * 可以logging.level.*=debug设置，*可以是包，也可以是某各类
+ * 日志级别：TRACE, DEBUG, INFO, WARN, ERROR, FATAL, OFF
  */
 @SpringBootApplication
 public class Application {
